96fb37cd3cf78ee4ca787c5a58adca85adf413df
cath0615 <md0615>
2021-07-18 20:48:27 +0800

    set up

---
+++ src/test/java/catherine/recipe/project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package catherine.recipe.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
