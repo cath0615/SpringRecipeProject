21d2b9469019a90dad112b49be1d96af5f18f07c
cath0615 <md0615>
2021-07-22 00:55:20 +0800

    add convenience method for bidirectional relationship

--- src/main/java/catherine/recipe/project/domain/Recipe.java
+++ src/main/java/catherine/recipe/project/domain/Recipe.java
@@ -131,9 +131,17 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
-    public Set<Ingredient> getIngredients() {
+    public Recipe addIngredient(Ingredient ingredient) {
+        //build the bidirectional method
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
+    }
+
+    public Set<Ingredient> addIngredient() {
         return ingredients;
     }
 
