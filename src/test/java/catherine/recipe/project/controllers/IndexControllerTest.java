3d132f2f9f684ffbde3323441cf0940f036cacba
cath0615 <md0615>
2021-07-26 15:16:47 +0800

    add spring MockMVC test

--- src/test/java/catherine/recipe/project/controllers/IndexControllerTest.java
+++ src/test/java/catherine/recipe/project/controllers/IndexControllerTest.java
@@ -7,16 +7,20 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.logging.Handler;
 
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class IndexControllerTest {
     IndexController indexController;
@@ -29,6 +33,18 @@ public class IndexControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
+    }
+
+    //build a spring mockMVC test, test spring MVC controller
+    @Test
+    public void testMockMVC() throws Exception {
+        //standalonesetup is faster than webAppContextSetUp since it brings up a spring context and
+        //our test will no longer be a unit test
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+        //get from root context and expect
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk()) //assert that we get back a 200 status from our controller
+                .andExpect(view().name("index"));
     }
 
     @Test
