911eed1430c87caf6ee28632abe221b56cefd758
cath0615 <md0615>
2021-07-26 13:54:42 +0800

    add JUnit test for category

--- src/test/java/catherine/recipe/project/ApplicationTests.java
+++ src/test/java/catherine/recipe/project/ApplicationTests.java
@@ -1,9 +1,13 @@
 package catherine.recipe.project;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class) //spring runner, bring up the spring context
 @SpringBootTest
+//this is an integration test
 class ApplicationTests {
 
 	@Test
---
+++ src/test/java/catherine/recipe/project/domain/CategoryTest.java
@@ -0,0 +1,31 @@
+package catherine.recipe.project.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CategoryTest {
+
+    Category category;
+
+    @Before
+    public void setUp() {
+        category = new Category(); //whenever we run, we will get a new category before each test object is executed
+    }
+
+    @Test
+    public void getId() {
+        Long idValue = 4L;
+        category.setId(idValue);
+        assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    public void getDescription() {
+    }
+
+    @Test
+    public void getRecipes() {
+    }
+}
