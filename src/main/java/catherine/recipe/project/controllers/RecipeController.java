6ef49f90687bbd640766b4742bbec5eebeb74d77
cath0615 <md0615>
2021-07-28 16:34:31 +0800

    create a new recipe on web

--- src/main/java/catherine/recipe/project/controllers/RecipeController.java
+++ src/main/java/catherine/recipe/project/controllers/RecipeController.java
@@ -1,10 +1,10 @@
 package catherine.recipe.project.controllers;
 
+import catherine.recipe.project.commands.RecipeCommand;
 import catherine.recipe.project.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by jt on 6/19/17.
@@ -23,5 +23,22 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    //implement the method that will render the view
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    //need to handle to post back
+    //@ModelAttribute: an annotation to tell spring to bind the form post parameters to the recipeCommand object
+    @PostMapping//tell spring when we get a post to the recipe url, invoke this specific method
+    @RequestMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+        //a command to tell spring MVC to redirect to a specific url
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
