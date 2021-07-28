0895b800f11b7233084c2c0428397a32436b1ec6
cath0615 <md0615>
2021-07-28 14:12:28 +0800

    adding command object

---
+++ src/main/java/catherine/recipe/project/commands/RecipeCommand.java
@@ -0,0 +1,31 @@
+package catherine.recipe.project.commands;
+
+import catherine.recipe.project.domain.Difficulty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+//gives us a single POJO
+//to get this in or out of the system, we want to do a type conversion
+//we need to covert from the type of the domain object back/over to the command object and vice-versa
+@Getter
+@Setter
+@NoArgsConstructor
+public class RecipeCommand {
+    private Long id;
+    private String description;
+    private Integer prepTime;
+    private Integer cookTime;
+    private Integer servings;
+    private String source;
+    private String url;
+    private String directions;
+    private Set<IngredientCommand> ingredients = new HashSet<>();
+    private Difficulty difficulty;
+    private NotesCommand notes;
+    private Set<CategoryCommand> categories = new HashSet<>();
+}
---
+++ src/main/java/catherine/recipe/project/commands/UnitOfMeasureCommand.java
@@ -0,0 +1,13 @@
+package catherine.recipe.project.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UnitOfMeasureCommand {
+    private Long id;
+    private String description;
+}
